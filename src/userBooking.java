fdabecabebba7d227630c077f8c26f5f20b344d7
VK <DEFCONone1@users.noreply.github.com>
2018-01-15 17:47:50 +0000

    Adds basic form

---
+++ src/userBooking.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class userBooking {
+    private JPanel panel1;
+    private JButton homeButton;
+    private JButton languageSelectButton;
+    private JButton displayTimetableButton;
+    private JButton destination1Button;
+    private JButton destination2Button;
+    private JButton destination3Button;
+    private JButton destination4Button;
+    private JButton destination5Button;
+    private JButton destination6Button;
+}
