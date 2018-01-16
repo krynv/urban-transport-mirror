1dd11871e40cd950c16c62438cb61d83aa767f13
VK <DEFCONone1@users.noreply.github.com>
2018-01-16 13:26:07 +0000

    Adds JFormDesigner form

--- src/userBooking.java
+++ src/userBooking.java
@@ -1,14 +1,51 @@
+import java.awt.*;
 import javax.swing.*;
-
+import com.intellij.uiDesigner.core.*;
 public class userBooking {
-    private JPanel panel1;
-    private JButton homeButton;
-    private JButton languageSelectButton;
-    private JButton displayTimetableButton;
-    private JButton destination1Button;
-    private JButton destination2Button;
-    private JButton destination3Button;
-    private JButton destination4Button;
-    private JButton destination5Button;
-    private JButton destination6Button;
+    public userBooking() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - godieina fire
+        panel2 = new JPanel();
+        JPanel panel1 = new JPanel();
+
+        //======== panel2 ========
+        {
+            panel2.setMinimumSize(new Dimension(500, 500));
+
+            // JFormDesigner evaluation mark
+            panel2.setBorder(new javax.swing.border.CompoundBorder(
+                new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
+                    "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
+                    javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
+                    java.awt.Color.red), panel2.getBorder())); panel2.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+
+            panel2.setLayout(new GridLayout());
+
+            //======== panel1 ========
+            {
+
+                GroupLayout panel1Layout = new GroupLayout(panel1);
+                panel1.setLayout(panel1Layout);
+                panel1Layout.setHorizontalGroup(
+                    panel1Layout.createParallelGroup()
+                        .addGap(0, 500, Short.MAX_VALUE)
+                );
+                panel1Layout.setVerticalGroup(
+                    panel1Layout.createParallelGroup()
+                        .addGap(0, 500, Short.MAX_VALUE)
+                );
+            }
+            panel2.add(panel1);
+        }
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - godieina fire
+    private JPanel panel2;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
