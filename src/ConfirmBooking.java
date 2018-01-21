5be18ff71c9d7b2d350af4d50f27564837fb3d01
VK <DEFCONone1@users.noreply.github.com>
2018-01-21 16:38:23 +0000

    Adds UI for Confirm Booking menu screen

---
+++ src/ConfirmBooking.java
@@ -0,0 +1,243 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import com.jgoodies.forms.layout.*;
+public class ConfirmBooking extends JFrame {
+    public ConfirmBooking() {
+        initComponents();
+        this.setContentPane(panel2);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+    }
+
+    private void homeActionPerformed(ActionEvent e) {
+        new UserBooking().setVisible(true);
+        this.dispose();
+    }
+
+    private void languageSelectActionPerformed(ActionEvent e) {
+        new SelectLanguage().setVisible(true);
+        this.dispose();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - godieina fire
+        panel2 = new JPanel();
+        home = new JButton();
+        languageSelect = new JButton();
+        displayTimetable = new JButton();
+        dateTime = new JLabel();
+        label1 = new JLabel();
+        label2 = new JLabel();
+        label3 = new JLabel();
+        label4 = new JLabel();
+        label5 = new JLabel();
+        label6 = new JLabel();
+        label7 = new JLabel();
+        checkBox1 = new JCheckBox();
+        checkBox2 = new JCheckBox();
+        textField1 = new JTextField();
+        label8 = new JLabel();
+        button1 = new JButton();
+        label9 = new JLabel();
+        label10 = new JLabel();
+
+        //======== panel2 ========
+        {
+
+            // JFormDesigner evaluation mark
+            panel2.setBorder(new javax.swing.border.CompoundBorder(
+                new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
+                    "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
+                    javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
+                    java.awt.Color.red), panel2.getBorder())); panel2.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+
+
+            //---- home ----
+            home.setText("Home");
+            home.addActionListener(e -> homeActionPerformed(e));
+
+            //---- languageSelect ----
+            languageSelect.setText("languageSelect");
+            languageSelect.addActionListener(e -> languageSelectActionPerformed(e));
+
+            //---- displayTimetable ----
+            displayTimetable.setText("displayTimetable");
+            displayTimetable.setEnabled(false);
+
+            //---- dateTime ----
+            dateTime.setText("dateTime");
+            dateTime.setHorizontalAlignment(SwingConstants.CENTER);
+            dateTime.setMaximumSize(new Dimension(25, 25));
+            dateTime.setMinimumSize(new Dimension(25, 25));
+            dateTime.setPreferredSize(new Dimension(25, 25));
+
+            //---- label1 ----
+            label1.setText("Destination");
+
+            //---- label2 ----
+            label2.setText("Station");
+
+            //---- label3 ----
+            label3.setText("Departure Date/Time");
+
+            //---- label4 ----
+            label4.setText("Ticket Information");
+
+            //---- label5 ----
+            label5.setText("Ticket Type");
+
+            //---- label6 ----
+            label6.setText("Price");
+
+            //---- label7 ----
+            label7.setText("Payment Method");
+
+            //---- checkBox1 ----
+            checkBox1.setText("CASH");
+
+            //---- checkBox2 ----
+            checkBox2.setText("CARD");
+
+            //---- label8 ----
+            label8.setText("Coupon Code");
+
+            //---- button1 ----
+            button1.setText("Advance");
+
+            //---- label9 ----
+            label9.setText("Return Date/Time");
+
+            //---- label10 ----
+            label10.setText("Via Locations");
+
+            GroupLayout panel2Layout = new GroupLayout(panel2);
+            panel2.setLayout(panel2Layout);
+            panel2Layout.setHorizontalGroup(
+                panel2Layout.createParallelGroup()
+                    .addGroup(panel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(panel2Layout.createParallelGroup()
+                            .addComponent(dateTime, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
+                            .addGroup(panel2Layout.createSequentialGroup()
+                                .addGroup(panel2Layout.createParallelGroup()
+                                    .addComponent(languageSelect, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(displayTimetable, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(home, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE))
+                                .addGroup(panel2Layout.createParallelGroup()
+                                    .addGroup(panel2Layout.createSequentialGroup()
+                                        .addGap(38, 38, 38)
+                                        .addGroup(panel2Layout.createParallelGroup()
+                                            .addGroup(panel2Layout.createSequentialGroup()
+                                                .addGap(2, 2, 2)
+                                                .addGroup(panel2Layout.createParallelGroup()
+                                                    .addGroup(panel2Layout.createSequentialGroup()
+                                                        .addComponent(label5)
+                                                        .addGap(60, 60, 60)
+                                                        .addComponent(label6))
+                                                    .addComponent(label7, GroupLayout.PREFERRED_SIZE, 112, GroupLayout.PREFERRED_SIZE)))
+                                            .addGroup(panel2Layout.createSequentialGroup()
+                                                .addGroup(panel2Layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                                                    .addGroup(panel2Layout.createParallelGroup()
+                                                        .addComponent(checkBox1)
+                                                        .addComponent(checkBox2))
+                                                    .addGroup(panel2Layout.createParallelGroup()
+                                                        .addComponent(label8)
+                                                        .addComponent(textField1, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE)))
+                                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 244, Short.MAX_VALUE)
+                                                .addComponent(button1))))
+                                    .addGroup(panel2Layout.createSequentialGroup()
+                                        .addGroup(panel2Layout.createParallelGroup()
+                                            .addGroup(panel2Layout.createSequentialGroup()
+                                                .addGap(130, 130, 130)
+                                                .addGroup(panel2Layout.createParallelGroup()
+                                                    .addGroup(panel2Layout.createSequentialGroup()
+                                                        .addGap(87, 87, 87)
+                                                        .addComponent(label10))
+                                                    .addGroup(panel2Layout.createSequentialGroup()
+                                                        .addComponent(label3, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)
+                                                        .addGap(0, 0, 0)
+                                                        .addComponent(label9))
+                                                    .addGroup(panel2Layout.createSequentialGroup()
+                                                        .addGap(68, 68, 68)
+                                                        .addComponent(label2)
+                                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                                        .addComponent(label1))))
+                                            .addGroup(panel2Layout.createSequentialGroup()
+                                                .addGap(27, 27, 27)
+                                                .addComponent(label4)))
+                                        .addGap(0, 98, Short.MAX_VALUE)))))
+                        .addGap(35, 35, 35))
+            );
+            panel2Layout.setVerticalGroup(
+                panel2Layout.createParallelGroup()
+                    .addGroup(panel2Layout.createSequentialGroup()
+                        .addGroup(panel2Layout.createParallelGroup()
+                            .addGroup(panel2Layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(home, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+                                .addGap(42, 42, 42)
+                                .addComponent(languageSelect, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+                                .addGap(6, 6, 6)
+                                .addComponent(displayTimetable, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))
+                            .addGroup(panel2Layout.createSequentialGroup()
+                                .addGap(47, 47, 47)
+                                .addGroup(panel2Layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                    .addComponent(label1)
+                                    .addComponent(label2))
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(panel2Layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                    .addComponent(label3, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(label9))
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(label10)
+                                .addGap(18, 18, 18)
+                                .addComponent(label4, GroupLayout.PREFERRED_SIZE, 16, GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(panel2Layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                    .addComponent(label5)
+                                    .addComponent(label6))
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(label7)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(checkBox1)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(checkBox2)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(label8)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(panel2Layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                    .addComponent(textField1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(button1))))
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 14, Short.MAX_VALUE)
+                        .addComponent(dateTime, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap())
+            );
+        }
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - godieina fire
+    private JPanel panel2;
+    private JButton home;
+    private JButton languageSelect;
+    private JButton displayTimetable;
+    private JLabel dateTime;
+    private JLabel label1;
+    private JLabel label2;
+    private JLabel label3;
+    private JLabel label4;
+    private JLabel label5;
+    private JLabel label6;
+    private JLabel label7;
+    private JCheckBox checkBox1;
+    private JCheckBox checkBox2;
+    private JTextField textField1;
+    private JLabel label8;
+    private JButton button1;
+    private JLabel label9;
+    private JLabel label10;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
