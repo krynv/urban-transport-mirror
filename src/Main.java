e3bb7fa9f8ff7135c2b19efaaca5c1d313d52638
Daniel <djmorris94@gmail.com>
2018-01-22 04:31:31 +0000

    Implemented Main navigation menu
    Added main three menu option aswell as reports sub menu options.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
 
-    public static void main(String[] args) {
+public class Main extends JFrame {
+
+    public static void main(String[] args, Container mainPanel) {
         System.out.println("Hello World!");
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                test GUI = new test(mainPanel);
+                GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                GUI.setVisible(true);
+            }
+        });
     }
 }
