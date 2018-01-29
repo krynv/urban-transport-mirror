c4bec76f3bc79515a5f45cecfad5c80409f9309c
benjaminw4rd <ben.ward75@yahoo.co.uk>
2018-01-29 12:13:21 +0000

    added test file

---
+++ src/test/java/LoginRegisterTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginRegisterTest {
+
+    @Test
+    public void exampleTest() {
+        assertTrue(true);
+    }
+
+}
