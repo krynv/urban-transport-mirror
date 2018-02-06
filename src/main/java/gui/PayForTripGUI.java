eded0cba9236f3258304030838676036da2775c5
VK <DEFCONone1@users.noreply.github.com>
2018-02-06 20:15:36 +0000

    Adds Select Fares screen

--- src/main/java/gui/PayForTripGUI.java
+++ src/main/java/gui/PayForTripGUI.java
@@ -2,7 +2,9 @@ package gui;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.time.LocalDateTime;
 import javax.swing.*;
+import javax.swing.event.*;
 import com.jgoodies.forms.layout.*;
 
 public class PayForTripGUI extends JFrame {
@@ -16,18 +18,21 @@ public class PayForTripGUI extends JFrame {
         this.setLocationRelativeTo(null);
         this.setContentPane(pnlMain);
         this.setTitle("Home");
-        pnlSelectLanguage.setVisible(false);
+        setAllToFalse();
         pnlHome.setVisible(true);
         this.pack();
     }
 
     private void btnSelectLanguageActionPerformed(ActionEvent e) {
-        pnlHome.setVisible(false);
+        setAllToFalse();
         pnlSelectLanguage.setVisible(true);
+        this.setTitle("Select Language");
     }
 
     private void btnDestination1ActionPerformed(ActionEvent e) {
-        // TODO add your code here
+        setAllToFalse();
+        pnlSelectFares.setVisible(true);
+        this.setTitle("Select Fares");
     }
 
     //TODO: class diagram says it doesn't accept parameters??
@@ -37,8 +42,15 @@ public class PayForTripGUI extends JFrame {
     }
 
     private void btnHomeActionPerformed(ActionEvent e) {
-        pnlSelectLanguage.setVisible(false);
+        setAllToFalse();
         pnlHome.setVisible(true);
+        this.setTitle("Home");
+    }
+
+    private void setAllToFalse() {
+        pnlSelectLanguage.setVisible(false);
+        pnlSelectFares.setVisible(false);
+        pnlHome.setVisible(false);
     }
 
     /* ----- Select Language ----- */
@@ -60,6 +72,30 @@ public class PayForTripGUI extends JFrame {
             loadLanguageFile(languageButtonPressed);
             pnlSelectLanguage.setVisible(false);
             pnlHome.setVisible(true);
+            this.setTitle("Home");
+        }
+        else { // default to english
+            languageButtonPressed = "English";
+            loadLanguageFile(languageButtonPressed);
+            pnlSelectLanguage.setVisible(false);
+            pnlHome.setVisible(true);
+            this.setTitle("Home");
+        }
+    }
+
+    private void searchActionPerformed(ActionEvent e) {
+        LocalDateTime departureDateTime = LocalDateTime.of(2018,2,4,12,0);
+        LocalDateTime arrivalDateTime = LocalDateTime.of(2018,2,4,13,0);
+
+
+    }
+
+    private void ckbxOneWayStateChanged(ChangeEvent e) {
+        if (ckbxOneWay.isSelected()) {
+            dpdnReturn.setEnabled(false);
+        }
+        else {
+            dpdnReturn.setEnabled(true);
         }
     }
 
@@ -88,6 +124,17 @@ public class PayForTripGUI extends JFrame {
         btnDestination2 = new JButton();
         btnDestination3 = new JButton();
         btnDestination6 = new JButton();
+        pnlSelectFares = new JPanel();
+        btnSearch = new JButton();
+        ckbxOpenReturn = new JCheckBox();
+        ckbxReturn = new JCheckBox();
+        ckbxOneWay = new JCheckBox();
+        lblTicketType = new JLabel();
+        dpdnDeparture = new JComboBox<>();
+        dpdnReturn = new JComboBox<>();
+        lblDepartureDateTime = new JLabel();
+        lblReturnDateTime = new JLabel();
+        lblDestination = new JLabel();
 
         //======== pnlMain ========
         {
@@ -264,6 +311,106 @@ public class PayForTripGUI extends JFrame {
                         );
                     }
                     pnlContent.add(pnlHome, "card1");
+
+                    //======== pnlSelectFares ========
+                    {
+
+                        //---- btnSearch ----
+                        btnSearch.setText("Search");
+                        btnSearch.addActionListener(e -> searchActionPerformed(e));
+
+                        //---- ckbxOpenReturn ----
+                        ckbxOpenReturn.setText("Open Return");
+
+                        //---- ckbxReturn ----
+                        ckbxReturn.setText("Return");
+
+                        //---- ckbxOneWay ----
+                        ckbxOneWay.setText("One Way");
+                        ckbxOneWay.addChangeListener(e -> ckbxOneWayStateChanged(e));
+
+                        //---- lblTicketType ----
+                        lblTicketType.setText("Ticket Type");
+
+                        //---- dpdnDeparture ----
+                        dpdnDeparture.setModel(new DefaultComboBoxModel<>(new String[] {
+                            "12:00"
+                        }));
+
+                        //---- dpdnReturn ----
+                        dpdnReturn.setModel(new DefaultComboBoxModel<>(new String[] {
+                            "13:00"
+                        }));
+
+                        //---- lblDepartureDateTime ----
+                        lblDepartureDateTime.setText("Departure Date/Time");
+
+                        //---- lblReturnDateTime ----
+                        lblReturnDateTime.setText("Return Date/Time");
+
+                        //---- lblDestination ----
+                        lblDestination.setText("London");
+
+                        GroupLayout pnlSelectFaresLayout = new GroupLayout(pnlSelectFares);
+                        pnlSelectFares.setLayout(pnlSelectFaresLayout);
+                        pnlSelectFaresLayout.setHorizontalGroup(
+                            pnlSelectFaresLayout.createParallelGroup()
+                                .addGroup(pnlSelectFaresLayout.createParallelGroup()
+                                    .addGroup(pnlSelectFaresLayout.createSequentialGroup()
+                                        .addGap(0, 0, Short.MAX_VALUE)
+                                        .addGroup(pnlSelectFaresLayout.createParallelGroup()
+                                            .addGroup(pnlSelectFaresLayout.createSequentialGroup()
+                                                .addGap(112, 112, 112)
+                                                .addComponent(lblDestination))
+                                            .addGroup(pnlSelectFaresLayout.createSequentialGroup()
+                                                .addComponent(lblDepartureDateTime)
+                                                .addGap(63, 63, 63)
+                                                .addComponent(lblReturnDateTime, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE))
+                                            .addGroup(pnlSelectFaresLayout.createSequentialGroup()
+                                                .addComponent(dpdnDeparture, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
+                                                .addGap(63, 63, 63)
+                                                .addComponent(dpdnReturn, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE))
+                                            .addComponent(lblTicketType)
+                                            .addComponent(ckbxOneWay, GroupLayout.PREFERRED_SIZE, 102, GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(ckbxReturn, GroupLayout.PREFERRED_SIZE, 102, GroupLayout.PREFERRED_SIZE)
+                                            .addGroup(pnlSelectFaresLayout.createSequentialGroup()
+                                                .addComponent(ckbxOpenReturn)
+                                                .addGap(191, 191, 191)
+                                                .addComponent(btnSearch)))
+                                        .addGap(0, 0, Short.MAX_VALUE)))
+                                .addGap(0, 545, Short.MAX_VALUE)
+                        );
+                        pnlSelectFaresLayout.setVerticalGroup(
+                            pnlSelectFaresLayout.createParallelGroup()
+                                .addGroup(pnlSelectFaresLayout.createParallelGroup()
+                                    .addGroup(pnlSelectFaresLayout.createSequentialGroup()
+                                        .addGap(0, 0, Short.MAX_VALUE)
+                                        .addComponent(lblDestination)
+                                        .addGap(28, 28, 28)
+                                        .addGroup(pnlSelectFaresLayout.createParallelGroup()
+                                            .addComponent(lblDepartureDateTime, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(lblReturnDateTime, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE))
+                                        .addGap(6, 6, 6)
+                                        .addGroup(pnlSelectFaresLayout.createParallelGroup()
+                                            .addComponent(dpdnDeparture, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(dpdnReturn, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                                        .addGap(17, 17, 17)
+                                        .addComponent(lblTicketType)
+                                        .addGap(6, 6, 6)
+                                        .addComponent(ckbxOneWay)
+                                        .addGap(6, 6, 6)
+                                        .addComponent(ckbxReturn)
+                                        .addGap(6, 6, 6)
+                                        .addGroup(pnlSelectFaresLayout.createParallelGroup()
+                                            .addComponent(ckbxOpenReturn)
+                                            .addGroup(pnlSelectFaresLayout.createSequentialGroup()
+                                                .addGap(6, 6, 6)
+                                                .addComponent(btnSearch)))
+                                        .addGap(0, 0, Short.MAX_VALUE)))
+                                .addGap(0, 458, Short.MAX_VALUE)
+                        );
+                    }
+                    pnlContent.add(pnlSelectFares, "card3");
                 }
 
                 GroupLayout pnlSideLayout = new GroupLayout(pnlSide);
@@ -327,5 +474,16 @@ public class PayForTripGUI extends JFrame {
     private JButton btnDestination2;
     private JButton btnDestination3;
     private JButton btnDestination6;
+    private JPanel pnlSelectFares;
+    private JButton btnSearch;
+    private JCheckBox ckbxOpenReturn;
+    private JCheckBox ckbxReturn;
+    private JCheckBox ckbxOneWay;
+    private JLabel lblTicketType;
+    private JComboBox<String> dpdnDeparture;
+    private JComboBox<String> dpdnReturn;
+    private JLabel lblDepartureDateTime;
+    private JLabel lblReturnDateTime;
+    private JLabel lblDestination;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
