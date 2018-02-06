0c1e6d40524b5d181528fcac72cb2d72bcb22baa
VK <DEFCONone1@users.noreply.github.com>
2018-02-06 17:39:10 +0000

    Fixes content panels

--- src/main/java/gui/PayForTripGUI.java
+++ src/main/java/gui/PayForTripGUI.java
@@ -35,17 +35,19 @@ public class PayForTripGUI extends JFrame {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - Barry Chuckle
         pnlMain = new JPanel();
-        pnlHome = new JPanel();
+        pnlSide = new JPanel();
         btnHome = new JButton();
         btnSelectLanguage = new JButton();
         btnDisplayTimetable = new JButton();
         lblDateTime = new JLabel();
-        btnDestination4 = new JButton();
+        pnlContent = new JPanel();
+        pnlHome = new JPanel();
         btnDestination1 = new JButton();
+        btnDestination4 = new JButton();
         btnDestination5 = new JButton();
         btnDestination2 = new JButton();
-        btnDestination6 = new JButton();
         btnDestination3 = new JButton();
+        btnDestination6 = new JButton();
 
         //======== pnlMain ========
         {
@@ -59,7 +61,7 @@ public class PayForTripGUI extends JFrame {
 
             pnlMain.setLayout(new CardLayout());
 
-            //======== pnlHome ========
+            //======== pnlSide ========
             {
 
                 //---- btnHome ----
@@ -80,84 +82,115 @@ public class PayForTripGUI extends JFrame {
                 lblDateTime.setMinimumSize(new Dimension(25, 25));
                 lblDateTime.setPreferredSize(new Dimension(25, 25));
 
-                //---- btnDestination4 ----
-                btnDestination4.setText("Destination 4");
-                btnDestination4.setFont(new Font(".SF NS Text", Font.PLAIN, 10));
+                //======== pnlContent ========
+                {
+                    pnlContent.setLayout(new CardLayout());
 
-                //---- btnDestination1 ----
-                btnDestination1.setText("London");
-                btnDestination1.setFont(new Font(".SF NS Text", Font.PLAIN, 10));
-                btnDestination1.addActionListener(e -> btnDestination1ActionPerformed(e));
+                    //======== pnlHome ========
+                    {
 
-                //---- btnDestination5 ----
-                btnDestination5.setText("Destination 5");
-                btnDestination5.setFont(new Font(".SF NS Text", Font.PLAIN, 10));
+                        //---- btnDestination1 ----
+                        btnDestination1.setText("London");
+                        btnDestination1.setFont(new Font(".SF NS Text", Font.PLAIN, 10));
+                        btnDestination1.addActionListener(e -> btnDestination1ActionPerformed(e));
 
-                //---- btnDestination2 ----
-                btnDestination2.setText("Destination 2");
-                btnDestination2.setFont(new Font(".SF NS Text", Font.PLAIN, 10));
+                        //---- btnDestination4 ----
+                        btnDestination4.setText("Destination 4");
+                        btnDestination4.setFont(new Font(".SF NS Text", Font.PLAIN, 10));
 
-                //---- btnDestination6 ----
-                btnDestination6.setText("Destination 6");
-                btnDestination6.setFont(new Font(".SF NS Text", Font.PLAIN, 10));
+                        //---- btnDestination5 ----
+                        btnDestination5.setText("Destination 5");
+                        btnDestination5.setFont(new Font(".SF NS Text", Font.PLAIN, 10));
 
-                //---- btnDestination3 ----
-                btnDestination3.setText("Destination 3");
-                btnDestination3.setFont(new Font(".SF NS Text", Font.PLAIN, 10));
+                        //---- btnDestination2 ----
+                        btnDestination2.setText("Destination 2");
+                        btnDestination2.setFont(new Font(".SF NS Text", Font.PLAIN, 10));
 
-                GroupLayout pnlHomeLayout = new GroupLayout(pnlHome);
-                pnlHome.setLayout(pnlHomeLayout);
-                pnlHomeLayout.setHorizontalGroup(
-                    pnlHomeLayout.createParallelGroup()
-                        .addGroup(pnlHomeLayout.createSequentialGroup()
-                            .addGroup(pnlHomeLayout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
-                                .addComponent(btnHome, GroupLayout.DEFAULT_SIZE, 143, Short.MAX_VALUE)
-                                .addComponent(btnDisplayTimetable, GroupLayout.DEFAULT_SIZE, 143, Short.MAX_VALUE)
-                                .addComponent(btnSelectLanguage, GroupLayout.DEFAULT_SIZE, 143, Short.MAX_VALUE)
-                                .addComponent(lblDateTime, GroupLayout.DEFAULT_SIZE, 143, Short.MAX_VALUE))
-                            .addGap(106, 106, 106)
-                            .addGroup(pnlHomeLayout.createParallelGroup()
-                                .addGroup(pnlHomeLayout.createSequentialGroup()
-                                    .addComponent(btnDestination1, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
-                                    .addGap(12, 12, 12)
-                                    .addComponent(btnDestination2, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
-                                    .addGap(12, 12, 12)
-                                    .addComponent(btnDestination3, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE))
-                                .addGroup(pnlHomeLayout.createSequentialGroup()
-                                    .addComponent(btnDestination4, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
-                                    .addGap(12, 12, 12)
-                                    .addComponent(btnDestination5, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
-                                    .addGap(12, 12, 12)
-                                    .addComponent(btnDestination6, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)))
-                            .addContainerGap(127, Short.MAX_VALUE))
+                        //---- btnDestination3 ----
+                        btnDestination3.setText("Destination 3");
+                        btnDestination3.setFont(new Font(".SF NS Text", Font.PLAIN, 10));
+
+                        //---- btnDestination6 ----
+                        btnDestination6.setText("Destination 6");
+                        btnDestination6.setFont(new Font(".SF NS Text", Font.PLAIN, 10));
+
+                        GroupLayout pnlHomeLayout = new GroupLayout(pnlHome);
+                        pnlHome.setLayout(pnlHomeLayout);
+                        pnlHomeLayout.setHorizontalGroup(
+                            pnlHomeLayout.createParallelGroup()
+                                .addGroup(pnlHomeLayout.createParallelGroup()
+                                    .addGroup(pnlHomeLayout.createSequentialGroup()
+                                        .addGap(0, 0, Short.MAX_VALUE)
+                                        .addGroup(pnlHomeLayout.createParallelGroup()
+                                            .addGroup(pnlHomeLayout.createSequentialGroup()
+                                                .addComponent(btnDestination1, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+                                                .addGap(12, 12, 12)
+                                                .addComponent(btnDestination2, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+                                                .addGap(12, 12, 12)
+                                                .addComponent(btnDestination3, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE))
+                                            .addGroup(pnlHomeLayout.createSequentialGroup()
+                                                .addComponent(btnDestination4, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+                                                .addGap(12, 12, 12)
+                                                .addComponent(btnDestination5, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+                                                .addGap(12, 12, 12)
+                                                .addComponent(btnDestination6, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)))
+                                        .addGap(0, 0, Short.MAX_VALUE)))
+                                .addGap(0, 545, Short.MAX_VALUE)
+                        );
+                        pnlHomeLayout.setVerticalGroup(
+                            pnlHomeLayout.createParallelGroup()
+                                .addGroup(pnlHomeLayout.createParallelGroup()
+                                    .addGroup(pnlHomeLayout.createSequentialGroup()
+                                        .addGap(0, 0, Short.MAX_VALUE)
+                                        .addGroup(pnlHomeLayout.createParallelGroup()
+                                            .addComponent(btnDestination1, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(btnDestination2, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(btnDestination3, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE))
+                                        .addGap(33, 33, 33)
+                                        .addGroup(pnlHomeLayout.createParallelGroup()
+                                            .addComponent(btnDestination4, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(btnDestination5, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(btnDestination6, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE))
+                                        .addGap(0, 0, Short.MAX_VALUE)))
+                                .addGap(0, 458, Short.MAX_VALUE)
+                        );
+                    }
+                    pnlContent.add(pnlHome, "card1");
+                }
+
+                GroupLayout pnlSideLayout = new GroupLayout(pnlSide);
+                pnlSide.setLayout(pnlSideLayout);
+                pnlSideLayout.setHorizontalGroup(
+                    pnlSideLayout.createParallelGroup()
+                        .addGroup(pnlSideLayout.createSequentialGroup()
+                            .addGroup(pnlSideLayout.createParallelGroup()
+                                .addComponent(btnHome, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(btnSelectLanguage, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(btnDisplayTimetable, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(lblDateTime, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE))
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(pnlContent, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addContainerGap())
                 );
-                pnlHomeLayout.setVerticalGroup(
-                    pnlHomeLayout.createParallelGroup()
-                        .addGroup(pnlHomeLayout.createSequentialGroup()
-                            .addComponent(btnHome, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
-                            .addGroup(pnlHomeLayout.createParallelGroup()
-                                .addGroup(pnlHomeLayout.createSequentialGroup()
-                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 59, Short.MAX_VALUE)
+                pnlSideLayout.setVerticalGroup(
+                    pnlSideLayout.createParallelGroup()
+                        .addGroup(pnlSideLayout.createSequentialGroup()
+                            .addGroup(pnlSideLayout.createParallelGroup()
+                                .addGroup(GroupLayout.Alignment.TRAILING, pnlSideLayout.createSequentialGroup()
+                                    .addComponent(btnHome, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                     .addComponent(btnSelectLanguage, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
-                                    .addGap(30, 30, 30)
+                                    .addGap(18, 18, 18)
                                     .addComponent(btnDisplayTimetable, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
-                                    .addGap(141, 141, 141))
-                                .addGroup(pnlHomeLayout.createSequentialGroup()
-                                    .addGap(46, 46, 46)
-                                    .addGroup(pnlHomeLayout.createParallelGroup()
-                                        .addComponent(btnDestination1, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(btnDestination2, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(btnDestination3, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE))
-                                    .addGap(33, 33, 33)
-                                    .addGroup(pnlHomeLayout.createParallelGroup()
-                                        .addComponent(btnDestination4, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(btnDestination5, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(btnDestination6, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE))
-                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 71, Short.MAX_VALUE)))
-                            .addComponent(lblDateTime, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))
+                                    .addGap(143, 143, 143)
+                                    .addComponent(lblDateTime, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))
+                                .addGroup(pnlSideLayout.createSequentialGroup()
+                                    .addContainerGap()
+                                    .addComponent(pnlContent, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                            .addContainerGap())
                 );
             }
-            pnlMain.add(pnlHome, "card1");
+            pnlMain.add(pnlSide, "card2");
         }
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
@@ -165,16 +198,18 @@ public class PayForTripGUI extends JFrame {
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     // Generated using JFormDesigner Evaluation license - Barry Chuckle
     private JPanel pnlMain;
-    private JPanel pnlHome;
+    private JPanel pnlSide;
     private JButton btnHome;
     private JButton btnSelectLanguage;
     private JButton btnDisplayTimetable;
     private JLabel lblDateTime;
-    private JButton btnDestination4;
+    private JPanel pnlContent;
+    private JPanel pnlHome;
     private JButton btnDestination1;
+    private JButton btnDestination4;
     private JButton btnDestination5;
     private JButton btnDestination2;
-    private JButton btnDestination6;
     private JButton btnDestination3;
+    private JButton btnDestination6;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
